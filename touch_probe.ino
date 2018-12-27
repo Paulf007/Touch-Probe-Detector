39276347e03f9e902773ebe5f9ec12862aea8465
Paulf007 <paul@paulf.co.za>
2018-12-27 17:06:00 +0200

    Update touch_probe.ino

--- touch_probe.ino
+++ touch_probe.ino
@@ -13,153 +13,162 @@ go off Indicating touch down.
 
 3 LED's is used to show the States of the system
 Red Led / Yellow Led - Blink   - shows error state - no probes are connected 
-Yellow Led           - Blink   - Compared value not set , need to push button
+GREEN Led           - Blink   - Compared value not set , need to push button
                     - Solid   - Value is set and are waiting to state change
                     - Off     - State has changed and you have touch down
-Green Led and Buz   - Indicate button press to set state
+YELLO Led and Buz   - Indicate button press to set state
                     - Solid state has changed and touch down is achieved.
 */
+// SETUP LED
+int LEDR = 11;
+int LEDY = 9;
+int LEDG = 10;
+int buttonPin = 12; 
+int buttonState = 0;
 
-// -------------- Calculation Value for the Resistance -----------
-int Vin=5; //voltage at 5V pin of arduino
-float Vout=0; //voltage at A0 pin of arduino
-float R1=4700; //value of known resistance
-float R2=0; //value of unknown resistance
-int a2d_data=0; 
-float buffer=0;
-float open_ct=2401700.00; // The value that is shown if the circuit is open.
+int analogPin= 2;
+int raw= 0;
+int Vin= 5;
+float Vout= 0;
+float R1= 4700;
+float R2= 0;
+float buffer= 0;
+int a2d_data=0;
+float open_ct=13000.00;
+float oper_ct=10.00;
+float setval = 0;
 
-// ------------ Setup to monitor the peak value at a certin time interval and then report if it chnages. 
-int peakValue = 0;
-int threshold = 500; 
-int noise = 0;
-int setval = 0;
-long lastMsg = 0;
-char msg[50];
-int value = 0;
+void setup()
+{
+Serial.begin(115200);
+//pinMode(buttonPin, INPUT_PULLUP); 
+pinMode(buttonPin,HIGH);
+pinMode(LEDG, OUTPUT);
+digitalWrite(LEDG,LOW);
+pinMode(LEDY, OUTPUT);
+digitalWrite(LEDY,LOW);
 
-const int8_t buzzer = 8;
-const int8_t led_yl = 9;
-const int8_t led_gr = 10;
-const int8_t led_rd = 11;
-const int8_t btn_push = 12;
 
-void setup() {
- Serial.begin(115200); 
-    // Turn LCD backlight on
-  pinMode(led_yl, OUTPUT);
-  digitalWrite(led_yl, HIGH);
-  delay(100); 
-  //digitalWrite(led_yl, LOW);
-  delay(100);
-  digitalWrite(led_yl, HIGH); 
-  pinMode(led_rd, OUTPUT);
-  digitalWrite(led_rd, HIGH);
-  delay(100);
-  //digitalWrite(led_rd, LOW);
-  delay(100);
-  digitalWrite(led_rd, HIGH);
-  pinMode(led_gr, OUTPUT);
-  digitalWrite(led_gr, HIGH);
-  delay(100);
-  //digitalWrite(led_gr, LOW);
-  delay(100);
-  digitalWrite(led_gr, HIGH);
-  pinMode(buzzer, OUTPUT);
-  digitalWrite(buzzer, HIGH);
-  digitalWrite(led_rd, HIGH);
-  digitalWrite(led_yl, HIGH);
-  digitalWrite(led_gr, HIGH);
-  delay(400);
-  digitalWrite(buzzer, LOW);
-   digitalWrite(led_rd,LOW);
-  digitalWrite(led_yl, LOW);
-  digitalWrite(led_gr, LOW);
-  pinMode(btn_push, INPUT);
+digitalWrite(LEDR,HIGH);
+digitalWrite(LEDG,LOW);
+digitalWrite(LEDY,LOW);
+delay(500);
+digitalWrite(LEDR,LOW);
+digitalWrite(LEDG,HIGH);
+digitalWrite(LEDY,LOW);
+delay(500);
+digitalWrite(LEDR,LOW);
+digitalWrite(LEDG,LOW);
+digitalWrite(LEDY,HIGH);
+delay(500);
+digitalWrite(LEDR,HIGH);
+digitalWrite(LEDG,HIGH);
+digitalWrite(LEDY,HIGH);
+delay(500);
+digitalWrite(LEDR,LOW);
+digitalWrite(LEDG,LOW);
+digitalWrite(LEDY,LOW);
 
 
 
-
-
+  
 }
 
+void loop()
+{
+// -------------------- Read Button State ---------------------
+buttonState = digitalRead(buttonPin);
 
-void loop() {
-
+// -------------------- Get Average Value of R2  --------------
  unsigned int x;
     unsigned int samples = 1024;
     unsigned long int accumulator = 0; // initialize accumulator
     for (x = 0; x < samples; x++) {
-        accumulator += analogRead(A0);
+        accumulator += analogRead(A2);
     }
-    
-// --------------- Compare Values --------------
-
 a2d_data=accumulator / samples;
 
 buffer=a2d_data*Vin;
 Vout=(buffer)/1024.0;
 buffer=Vout/(Vin-Vout); 
+raw= analogRead(analogPin);
 R2=R1*buffer;
-
-
-
+Serial.print("R2: ");
+Serial.println(R2);
+//delay(10);
+// ---------- Check if Probes Connected -------
 if(R2 >= open_ct)
-{ digitalWrite(buzzer,LOW);
+{ digitalWrite(LEDR, HIGH);
+digitalWrite(LEDY,LOW);
   Serial.println(R2);
-  digitalWrite(led_rd, LOW);       
   Serial.println("Please Connect Probes");
   delay(500);                  
-  digitalWrite(led_rd, HIGH);       
+  digitalWrite(LEDR, LOW); 
+  digitalWrite(LEDY,HIGH);     
   Serial.println("Please Connect Probes");
+  Serial.println(R2);
   delay(500);  
-  digitalWrite(led_rd, LOW);   
-  Serial.println("Please Connect Probes");
-  delay(500); 
-  digitalWrite(led_rd, LOW);
-  Serial.println("Please Connect Probes");
-
-}
-else
-{
-digitalWrite(led_rd, LOW);
+}else {
+// ------------- Check id Resisatnce is big enough ----------
+if(R2 <= oper_ct && setval == 0 )
+{ digitalWrite(LEDR, HIGH);
+digitalWrite(LEDG,LOW);
+  Serial.println(R2);
+  Serial.println("Please Turn Spindle");
+  delay(100);                  
+  digitalWrite(LEDR, LOW); 
+  digitalWrite(LEDG,HIGH);     
+  Serial.println("Please Turn Spindle");
+  Serial.println(R2);
+  delay(100);  
+}else {
+  
+// ------------- Check the Sate of the Button ---------------
 int sensorValue = R2;
-int buttonState = digitalRead(btn_push); //read the state of the button input 
-if (buttonState == LOW) { //pressing the button will produce a LOW state 0V 
-digitalWrite(led_gr,HIGH); //the led with turn on 
+ 
+if (buttonState == HIGH) { //pressing the button will produce a LOW state 0V 
+digitalWrite(LEDG,HIGH); //the led with turn on 
 Serial.println("Button Trigger");
-setval = R2 ;
+setval = R2;
 Serial.println(R2); 
-} else{ 
-digitalWrite(led_gr,LOW); //the led with turn off 
-} 
-
+}else {
+ digitalWrite(LEDG,LOW); 
+ digitalWrite(LEDY,LOW);
+ Serial.println(R2);
 }
 
 // -------------- Compare Set Val with Current Val
 if (setval > 0 ){
-  int checkval = R2 + noise ;
-  if (checkval < setval ){
+  float checkval = R2 ;
+  if (checkval < (setval ) ){
     Serial.println("State Changed");
-    digitalWrite(led_gr,HIGH); //the led with turn on
-    digitalWrite(led_yl,LOW);
-    digitalWrite(buzzer,HIGH); 
-    Serial.println(R2);
+    digitalWrite(LEDY,HIGH); //the led with turn on
+    digitalWrite(LEDG,LOW);
+    //digitalWrite(buzzer,HIGH);
+    Serial.println(setval); 
+    Serial.println(checkval);
   }else{
   Serial.println("Waiting for Sate");
-  digitalWrite(led_yl,HIGH);
-  digitalWrite(buzzer,LOW);
+  digitalWrite(LEDG,HIGH);
+  //digitalWrite(buzzer,LOW);
   Serial.println(setval);
-  Serial.println(R2);
+  Serial.println(checkval);
   }
 }else{
  Serial.println("State Not Set"); 
- digitalWrite(led_yl,HIGH);
- delay(500); 
- digitalWrite(led_yl,LOW);
- delay(500);
+ Serial.println(R2);
+ digitalWrite(LEDG,HIGH);
+ delay(100); 
+ digitalWrite(LEDG,LOW);
+ delay(100);
 }
-  
-     }
+
+}
+}
+// ------------- Print Value ---------
+
+
+}
+
 
 
